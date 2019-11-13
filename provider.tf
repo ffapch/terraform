d853be1eea338c1b0e5bcbd842e402bc57a4a296
Andrey Dovbnya <43334366+ffapch@users.noreply.github.com>
2019-11-13 10:35:05 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIA27AKHJ3MW7TEWIGA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
